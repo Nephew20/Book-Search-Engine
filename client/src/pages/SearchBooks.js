d71a7b3371d4f258834bc898e7dfaf0ccb0e2e2c
Nephew20 <cofieldk20@gmail.com>
2023-06-21 22:49:34 -0400

    updated the savedBooks and searchBooks js code

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -9,8 +9,11 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+import {useMutation} from '@apollo/client'
+import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -27,6 +30,7 @@ const SearchBooks = () => {
     return () => saveBookIds(savedBookIds);
   });
 
+  const [saveBook, {error}] = useMutation(SAVE_BOOK)
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
